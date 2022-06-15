49d533bfe2d8bcedd62b5e32375734da09877161
Teddy <teddymczieuwa@gmail.com>
2022-06-15 16:52:28 -0700

    revamped footer.tsx from class based component to functional component

--- src/footer.tsx
+++ src/footer.tsx
@@ -6,62 +6,58 @@
 
 /// <reference path="./interfaces.d.ts"/>
 
-import  classNames from "classnames";
-import * as React from "react";
+import classNames from "classnames";
+import React, {FC} from "react";
 import { ALL_TODOS, ACTIVE_TODOS, COMPLETED_TODOS } from "./constants";
 import { Utils } from "./utils";
 
-class TodoFooter extends React.Component<ITodoFooterProps, {}> {
+const TodoFooter:FC<ITodoFooterProps> = ({completedCount, onClearCompleted, nowShowing, count}) => {
+  const activeTodoWord = Utils.pluralize(count, 'item')
+  let clearButton = null
 
-  public render() {
-    var activeTodoWord = Utils.pluralize(this.props.count, 'item');
-    var clearButton = null;
-
-    if (this.props.completedCount > 0) {
-      clearButton = (
-        <button
-          className="clear-completed"
-          onClick={this.props.onClearCompleted}>
-          Clear completed
-        </button>
-      );
-    }
-
-    const nowShowing = this.props.nowShowing;
-    return (
-      <footer className="footer">
-        <span className="todo-count">
-          <strong>{this.props.count}</strong> {activeTodoWord} left
-        </span>
-        <ul className="filters">
-          <li>
-            <a
-              href="#/"
-              className={classNames({selected: nowShowing === ALL_TODOS})}>
-                All
-            </a>
-          </li>
-          {' '}
-          <li>
-            <a
-              href="#/active"
-              className={classNames({selected: nowShowing === ACTIVE_TODOS})}>
-                Active
-            </a>
-          </li>
-          {' '}
-          <li>
-            <a
-              href="#/completed"
-              className={classNames({selected: nowShowing === COMPLETED_TODOS})}>
-                Completed
-            </a>
-          </li>
-        </ul>
-        {clearButton}
-      </footer>
+  if(completedCount > 0) {
+    clearButton = (
+      <button
+        className="clear-completed"
+        onClick={onClearCompleted}>
+        Clear completed
+      </button>
     );
   }
+
+  return (
+    <footer className="footer">
+    <span className="todo-count">
+      <strong>{count}</strong> {activeTodoWord} left
+    </span>
+    <ul className="filters">
+      <li>
+        <a
+          href="#/"
+          className={classNames({selected: nowShowing === ALL_TODOS})}>
+            All
+        </a>
+      </li>
+      {' '}
+      <li>
+        <a
+          href="#/active"
+          className={classNames({selected: nowShowing === ACTIVE_TODOS})}>
+            Active
+        </a>
+      </li>
+      {' '}
+      <li>
+        <a
+          href="#/completed"
+          className={classNames({selected: nowShowing === COMPLETED_TODOS})}>
+            Completed
+        </a>
+      </li>
+    </ul>
+    {clearButton}
+  </footer>
+  )
 }
 
 export { TodoFooter };
